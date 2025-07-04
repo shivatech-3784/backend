f03710365f1dccdff10b2f06d28b7952166dc257
shivatech-3784 <shivaprasadgudipally2003@gmail.com>
2025-07-04 19:50:20 +0530

    connect to mongoDB

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path:'./.env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import express from "express";
+
+const app = express();
+
+
+(async () =>{
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}` )
+        app.on("error", (error) => {
+            console.error("Error connecting to app:", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`app is listening to the port  ${process.env.PORT}`);
+        });
+
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        throw error;
+    }
+})()*/
